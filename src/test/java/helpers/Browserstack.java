2bd7824f18b76491e1a35137d72128e332257b92
Aleksandra Menskaia <alex@Aleksandras-MacBook-Pro.local>
2024-02-23 14:35:46 +0100

    gradle_ change

--- src/test/java/helpers/Browserstack.java
+++ src/test/java/helpers/Browserstack.java
@@ -1,16 +1,18 @@
 package helpers;
 
+import config.BrowserstackConfig;
+import org.aeonbits.owner.ConfigFactory;
+
 import static io.restassured.RestAssured.given;
 
-
 public class Browserstack {
+    private static final BrowserstackConfig config = ConfigFactory.create(BrowserstackConfig.class);
 
     public static String videoUrl(String sessionId) {
-
         String url = String.format("https://api.browserstack.com/app-automate/sessions/%s.json", sessionId);
 
         return given()
-                .auth().basic("bsuser_YyIVRj", "p5RVD74vPC45pYtpT8R5")
+                .auth().basic(config.browserstackUser(), config.browserstackKey())
                 .get(url)
                 .then()
                 .log().status()
